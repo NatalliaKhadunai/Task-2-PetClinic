9059f078ef82fa4800177b9d0a71ce540c5f8d12
Natallia.Khadunai <natallia.khadunai@thomsonreuters.com>
2019-04-28 13:50:16 +0300

    Added failed test again

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -59,4 +59,10 @@ public class ValidatorTests {
         assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
         assertThat(violation.getMessage()).isEqualTo("must not be empty");
     }
+
+    @Test
+    public void failTest() {
+        assertTrue(false);
+
+    }
 }
